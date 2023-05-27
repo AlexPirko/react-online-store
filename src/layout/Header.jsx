4189312360f0246dd14f4f627526e2f2c4046b5f
Alex Pirko <youzhny_foto@gmail.com>
2023-05-27 11:52:17 +0300

    add links

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,12 +2,18 @@ function Header() {
     return (
         <nav className='teal darken-1'>
             <div className='nav-wrapper'>
-                <a href='#' className='brand-logo'>
+                <a
+                    href='https://alexpirko.github.io/react-online-store'
+                    className='brand-logo'
+                    target='_blank'
+                    rel='noreferrer'>
                     Online Store
                 </a>
                 <ul id='nav-mobile' className='right hide-on-med-and-down'>
                     <li>
-                        <a href='!#'>Sass</a>
+                        <a href='https://github.com/AlexPirko/react-online-store' target='_blank' rel='noreferrer'>
+                            REPO
+                        </a>
                     </li>
                 </ul>
             </div>
